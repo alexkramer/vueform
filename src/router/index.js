b1d67576efe895e53ec5cd23b7029892f07a9084
Ian Walter <public@iankwalter.com>
2017-02-10 10:32:22 -0500

    Fixing mode prop placement

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,8 @@ import PatientForm from '../components/PatientForm'
 
 Vue.use(Router)
 
-export default new Router(mode: 'history', {
+export default new Router({
+  mode: 'history',
   routes: [
     { name: 'ContactForm', path: '/contact', component: ContactForm },
     { name: 'PatientForm', path: '/patient', component: PatientForm },
