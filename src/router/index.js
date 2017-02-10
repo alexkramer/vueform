568970ba7109b9abeb9159a17688599bd35e063c
Ian Walter <public@iankwalter.com>
2017-02-10 10:19:57 -0500

    Adding history mode back to router

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import PatientForm from '../components/PatientForm'
 
 Vue.use(Router)
 
-export default new Router({
+export default new Router(mode: 'history', {
   routes: [
     { name: 'ContactForm', path: '/contact', component: ContactForm },
     { name: 'PatientForm', path: '/patient', component: PatientForm },
